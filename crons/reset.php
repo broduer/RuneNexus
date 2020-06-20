384bcf78f14bab25c28dcd91ffe85985a8ef3698
KingFox <rune.evo2012@gmail.com>
2020-06-20 11:01:59 -0500

    Create reset.php
    added cronjob to reset vote counts at first of month

---
+++ crons/reset.php
@@ -0,0 +1,21 @@
+<?php
+    include "cron_init.php";
+    
+    $updated = 0;
+
+    writeLog("Started vote reset...");
+
+    $startTime = microtime(true);
+
+    $servers = Servers::get();
+    
+    foreach ($servers as $server) {
+        $server->votes = 0;
+        $server->save();
+        $updated++;
+    }
+
+    $endTime = microtime(true);
+    $elapsed = number_format($endTime - $startTime, 4);
+
+    writeLog("Reset $updated servers' vote counts. Executed in ".$elapsed."s!");

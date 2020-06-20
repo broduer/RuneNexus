1daf6ac6ce9967ca596bfa4690b8cc3ccc23efce
KingFox <rune.evo2012@gmail.com>
2020-06-20 08:02:36 -0500

    Finished payments + Login update
    Payments for sponsor block is done. Login page now includes terms of service/privacy policy. Also moved some things into a template

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -29,9 +29,22 @@ class IndexController extends Controller {
             $this->set("page_title", "Servers");
         }
 
+        $sponsors = Sponsors::select([
+                'sponsors.id',
+                'servers.title', 
+                'servers.website', 
+                'servers.discord_link', 
+                'servers.banner_url'
+            ])    
+            ->where('expires', '>', time())
+            ->where('servers.banner_url', '!=', null)
+            ->where('servers.website', '!=', null)
+            ->leftJoin("servers", "servers.id", "=", "sponsors.server_id")
+            ->get();
+
         $this->set("servers", $servers);
         $this->set("revisions", $revisions);
-        
+        $this->set("sponsors", $sponsors);
     	return true;
     }
 

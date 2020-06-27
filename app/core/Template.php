0d616ddb594c29e64de5e8b3501917d13cfbae08
KingFox <rune.evo2012@gmail.com>
2020-06-27 05:53:51 -0500

    Replacing Icons

--- app/core/Template.php
+++ app/core/Template.php
@@ -37,6 +37,12 @@ class Template extends FilesystemLoader {
                     return web_root.'public/img/banners/'.$banner;
                 }
             }));
+
+            $twig->addFunction(new \Twig\TwigFunction('icon', function ($name, $width = 16, $height = 16) {
+                return '<svg class="bi" width="'.$width.'" height="'.$height.'" fill="currentColor">
+                    <use xlink:href="public/img/bootstrap-icons.svg#'.$name.'"/>
+                </svg>';
+            }, ['is_safe' => ['html']]));
             
             $twig->addFunction(new \Twig\TwigFunction('api', function ($string) {
                 return api_url.'/'.$string;
